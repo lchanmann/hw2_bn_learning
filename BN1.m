9f55ea6ecd257f0cddc296a595d5ded3f8a8302c
adilalazzawi <alazzawi1979@gmail.com>
2015-03-05 18:57:18 -0600

    Adil's update

--- BN1.m
+++ BN1.m
@@ -1,29 +1,42 @@
-%
-% Get data and discretize
-%
+% Bayesian Networks No.1
+%%
+display('_______________________________________________________');
+display('                                                       ');
+display('             Programming Assignment #2                 ');
+display('               Bayesian Networks No.1                  ');
+display('_______________________________________________________');
+display(' ');
+%% Load the data set...
 Pd = dataset1001(:, 5);
-Xb = discretize(dataset1001(:, 1));
-Xh = discretize(dataset1001(:, 2));
-Xt = discretize(dataset1001(:, 3));
-
-%
-% Generate probabilities
-%
+%% Get the discretize
+if (x==1)
+   Xb = discretize(dataset1001(:, 1));
+   Xh = discretize(dataset1001(:, 2));
+   Xt = discretize(dataset1001(:, 3));
+elseif (x==2)   
+   Xb = discretize(dataset1004(:, 1));
+   Xh = discretize(dataset1004(:, 2));
+   Xt = discretize(dataset1004(:, 3));
+end
+%% Generate probabilities
 P_Pd = [sum(Pd == 1)/length(Pd); sum(Pd == 0)/length(Pd)];
 P_Xb_given_Pd = BN1.CPT(Xb, Pd);
 P_Xh_given_Pd = BN1.CPT(Xh, Pd);
 P_Xt_given_Pd = BN1.CPT(Xt, Pd);
 
-%
-% Predict P(Pd|Xb, Xh, Xt)
-%
-model = BN1.model(P_Pd, P_Xb_given_Pd, P_Xh_given_Pd, P_Xt_given_Pd);
+%% Predict P(Pd|Xb, Xh, Xt)
+   model = BN1.model(P_Pd, P_Xb_given_Pd, P_Xh_given_Pd, P_Xt_given_Pd);
 
 % P(pd = 1|Xb = M, Xh = M, Xt = M)
-model.predict(1, ['M' 'M' 'M'])
+  display('Compute the P(pd = 1|Xb = M, Xh = M, Xt = M) :');
+  model.predict(1, ['M' 'M' 'M'])
 
 % P(pd = 1) without any evidences
-model.predict(1, ['-' '-' '-'])
+  display('Compute the P(pd = 1) without any evidences : ');
+  model.predict(1, ['-' '-' '-'])
 
 % P(pd = 1|Xh = M)
-model.predict(1, ['-' 'M' '-'])
+  display('Compute the P(pd = 1|Xh = M) : ');
+  model.predict(1, ['-' 'M' '-'])
+
+  pause;
--- assignment_2.m
+++ assignment_2.m
@@ -1,21 +1,36 @@
-%
-% CS8750 Spring 2015
-% Artificial Intelligence II
-% University of Missouri-Columbia
-%
-% Programming Assignment 2
-%
-clc; clear; close all;
-
-dataset1001 = load('1001testingdata.csv');
-dataset1004 = load('1004testingdata.csv');
-
-%
-% BN 1
-%
+%==========================================================================
+% CS 8750 - Artificial Intelligence II...
+% Programming Assignment #2 ...
+% Adil Al-Azzawi ... ECE
+% Chanmann Lim   ... CS
+% Fernando Torre ... CS
+%==========================================================================
+%%
+clc;clear;
+t=0;
+while (t ~= 1)
+    clc;
+    display('_______________________________________________________');
+    display(' ');
+    display('             Choose Your Data set                      ');
+    display('_______________________________________________________');
+    display('             1: For Data set 1001                      ');
+    display('             2: For Data set 1004                      ');
+    display('_______________________________________________________');
+    display(' ');
+    x = input(' Select the Baysian Network that you want to implement : ');
+    if (x==1)||(x==2)
+        dataset1001 = load('1001testingdata.csv');
+        dataset1004 = load('1004testingdata.csv');
+        t=1;
+    else    
+        msgbox('Invalid Data_set No...', 'Error','error');
+        pause;
+        t=0;
+    end
+end
+%% using BN#1...
 BN1
 
-%
-% BN 3
-%
+%% using BN#3...
 BN3
