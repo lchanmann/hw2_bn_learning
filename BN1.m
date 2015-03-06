05cdc68014ce9e8a3ab97d33a0ab6a959833ae03
Mann <chanmannlim@gmail.com>
2015-03-06 15:27:54 -0600

    format display

--- BN1.m
+++ BN1.m
@@ -28,15 +28,16 @@ P_Xt_given_Pd = BN1.CPT(Xt, Pd);
    model = BN1.model(P_Pd, P_Xb_given_Pd, P_Xh_given_Pd, P_Xt_given_Pd);
 
 % P(pd = 1|Xb = M, Xh = M, Xt = M)
-  display('Compute the P(pd = 1|Xb = M, Xh = M, Xt = M) :');
-  model.predict(1, ['M' 'M' 'M'])
+  display([ 'P(pd = 1|Xb = M, Xh = M, Xt = M) = ' ...
+      num2str(model.predict(1, ['M' 'M' 'M'])) ]);
+  
 
 % P(pd = 1) without any evidences
-  display('Compute the P(pd = 1) without any evidences : ');
-  model.predict(1, ['-' '-' '-'])
+  display([ 'P(pd = 1) without any evidences = ' ...
+      num2str(model.predict(1, ['-' '-' '-'])) ]);
 
 % P(pd = 1|Xh = M)
-  display('Compute the P(pd = 1|Xh = M) : ');
-  model.predict(1, ['-' 'M' '-'])
+  display([ 'P(pd = 1|Xh = M) = ' ...
+      num2str(model.predict(1, ['-' 'M' '-'])) ]);
 
   pause;
