97197ddcd59517872def9265ca4181946a030fc3
Mann <chanmannlim@gmail.com>
2015-03-06 17:50:53 -0600

    confusion matrix

--- BN1.m
+++ BN1.m
@@ -25,19 +25,38 @@ display(P_Xh_given_Pd);
 display(P_Xt_given_Pd);
 
 %% Predict P(Pd|Xb, Xh, Xt)
-  model = BN1.model(P_Pd, P_Xb_given_Pd, P_Xh_given_Pd, P_Xt_given_Pd);
+model = BN1.model(P_Pd, P_Xb_given_Pd, P_Xh_given_Pd, P_Xt_given_Pd);
 
-  display('--------------------- Prediction ---------------------');
+display('--------------------- Prediction ---------------------');
 % P(pd = 1|Xb = M, Xh = M, Xt = M)
-  display([ 'P(pd = 1|Xb = M, Xh = M, Xt = M) = ' ...
-      num2str(model.predict(1, ['M' 'M' 'M'])) ]);
+display([ 'P(pd = 1|Xb = M, Xh = M, Xt = M) = ' ...
+    num2str(model.predict(1, ['M' 'M' 'M'])) ]);
 
 % P(pd = 1) without any evidences
-  display([ 'P(pd = 1) without any evidences = ' ...
-      num2str(model.predict(1, ['-' '-' '-'])) ]);
+display([ 'P(pd = 1) without any evidences = ' ...
+    num2str(model.predict(1, ['-' '-' '-'])) ]);
 
 % P(pd = 1|Xh = M)
-  display([ 'P(pd = 1|Xh = M) = ' ...
-      num2str(model.predict(1, ['-' 'M' '-'])) ]);
+display([ 'P(pd = 1|Xh = M) = ' ...
+    num2str(model.predict(1, ['-' 'M' '-'])) ]);
 
-  pause;
+%% Confusion matrix
+evidence = [Xb Xh Xt];
+data_size = size(evidence, 1);
+prediction = zeros(data_size, 1);
+
+for i = 1:data_size
+    prediction(i, 1) = model.predict(1, evidence(i, :));
+end
+% normalize the prediction 
+%   the person drink (Pd = 1) if prediction > 0.5,
+%   otherwise, the person doesn't drink (Pd = 0)
+predicted_Pd = prediction > 0.5;
+C = confusion(Pd, predicted_Pd);
+
+display(' ');
+display('--------------------- Confusion matrix ---------------------');
+display(C);
+
+%%
+pause;
