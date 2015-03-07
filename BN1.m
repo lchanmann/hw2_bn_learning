1eb6465c4c5d0132df4f36c4a7435bcfac1d69cf
adilalazzawi <alazzawi1979@gmail.com>
2015-03-06 19:59:21 -0600

    Add some comments

--- BN1.m
+++ BN1.m
@@ -1,5 +1,6 @@
 % Bayesian Networks No.1
 %%
+clc;
 display('_______________________________________________________');
 display('                                                       ');
 display('             Programming Assignment #2                 ');
@@ -24,19 +25,19 @@ P_Xb_given_Pd = BN1.CPT(Xb, Pd);
 P_Xh_given_Pd = BN1.CPT(Xh, Pd);
 P_Xt_given_Pd = BN1.CPT(Xt, Pd);
 
-%% Predict P(Pd|Xb, Xh, Xt)
+%% Predict P(Pd|Xb, Xh, Xt)...
    model = BN1.model(P_Pd, P_Xb_given_Pd, P_Xh_given_Pd, P_Xt_given_Pd);
 
-% P(pd = 1|Xb = M, Xh = M, Xt = M)
+%% P(pd = 1|Xb = M, Xh = M, Xt = M)...
   display('Compute the P(pd = 1|Xb = M, Xh = M, Xt = M) :');
   model.predict(1, ['M' 'M' 'M'])
 
-% P(pd = 1) without any evidences
+%% P(pd = 1) without any evidences
   display('Compute the P(pd = 1) without any evidences : ');
   model.predict(1, ['-' '-' '-'])
 
-% P(pd = 1|Xh = M)
+%% P(pd = 1|Xh = M)
   display('Compute the P(pd = 1|Xh = M) : ');
   model.predict(1, ['-' 'M' '-'])
-
+  display('Press Enter to continue ... ');
   pause;
--- BN3.m
+++ BN3.m
@@ -1,31 +1,31 @@
-%
-% Get data and discretize
-%
-Pd = dataset1001(:, 5);
-Xb = dataset1001(:, 1);
-Xh = dataset1001(:, 2);
-Xt = dataset1001(:, 3);
+% Bayesian Networks No.3...
+%%
+display('_______________________________________________________');
+display('                                                       ');
+display('             Programming Assignment #2                 ');
+display('               Bayesian Networks No.3                  ');
+display('_______________________________________________________');
+display(' ');
+%% Get data and discretize...
+    Pd = dataset1001(:, 5);
+    Xb = dataset1001(:, 1);
+    Xh = dataset1001(:, 2);
+    Xt = dataset1001(:, 3);
 
-%
-% Maximum likelihood estimation
-%
-P_Pd = [sum(Pd == 1)/length(Pd); sum(Pd == 0)/length(Pd)];
-P_Xb_given_Pd = mle(Xb, Pd);
-P_Xh_given_Pd = mle(Xh, Pd);
-P_Xt_given_Pd = mle(Xt, Pd);
+%% Maximum likelihood estimation...
+    P_Pd = [sum(Pd == 1)/length(Pd); sum(Pd == 0)/length(Pd)];
+    % mle Maximum likelihood estimator
+    P_Xb_given_Pd = mle(Xb, Pd);
+    P_Xh_given_Pd = mle(Xh, Pd);
+    P_Xt_given_Pd = mle(Xt, Pd);
 
-%
-% Predict P(Pd|Xb, Xh, Xt)
-%
-model = BN3.model(P_Pd, P_Xb_given_Pd, P_Xh_given_Pd, P_Xt_given_Pd);
+%% Predict P(Pd|Xb, Xh, Xt)
+    model = BN3.model(P_Pd, P_Xb_given_Pd, P_Xh_given_Pd, P_Xt_given_Pd);
 
 % P(pd = 1|Xb = 9)
-model.predict(1, [9 0 0])
+  display('Compute the P(pd = 1|Xb = 9) :'); 
+  model.predict(1, [9 0 0])
 
 % P(pd = 1|Xb = 9.3, Xh = 78.4, Xt = 36.3)
-model.predict(1, [9.3 78.4 36.3])
-
-%
-%
-%
-%
+  display('Compute the P(pd = 1|Xb = 9.3, Xh = 78.4, Xt = 36.3) :'); 
+  model.predict(1, [9.3 78.4 36.3])
