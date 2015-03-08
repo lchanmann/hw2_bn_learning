d0d4bf0dfde2d82a8babe41f129b3b54ae2f4968
Mann <chanmannlim@gmail.com>
2015-03-08 15:19:52 -0500

    fix bn1

--- BN1.m
+++ BN1.m
@@ -4,28 +4,13 @@ clc;
 display('_______________________________________________________');
 display('                                                       ');
 display('                Programming Assignment #2              ');
-<<<<<<< HEAD
-fprintf('                  Data set No: %d\n', d);
-display('_______________________________________________________');
-display(' ');
-display('_______________________________________________________');
-display('                                                       ');
-display('               Bayesian Networks No.1                  ');
-display('_______________________________________________________');
-display(' ');
-fprintf('                      Dataset No: %d\n', d);
-=======
-fprintf('                     Data set No: %d\n', d);
->>>>>>> origin/master
+fprintf('                    Dataset No: %d\n', d);
 display('_______________________________________________________');
 display('                                                       ');
 display('                 Bayesian Networks No.1                ');
 display('_______________________________________________________');
 display(' ');
-<<<<<<< HEAD
-=======
 
->>>>>>> origin/master
 %% Load the data and discretize
 Pd = dataset(:, 5);
 Xb = discretize(dataset(:, 1));
@@ -38,11 +23,6 @@ P_Xb_given_Pd = BN1.CPT(Xb, Pd);
 P_Xh_given_Pd = BN1.CPT(Xh, Pd);
 P_Xt_given_Pd = BN1.CPT(Xt, Pd);
 
-<<<<<<< HEAD
-%% Predict P(Pd|Xb, Xh, Xt)...
-model = BN1.model(P_Pd, P_Xb_given_Pd, P_Xh_given_Pd, P_Xt_given_Pd);
-=======
->>>>>>> origin/master
 display('--------------------- Parameters learned ---------------------');
 display(P_Xb_given_Pd);
 display(P_Xh_given_Pd);
@@ -63,48 +43,12 @@ end
 %   otherwise, the person doesn't drink (Pd = 0)
 predicted_Pd = prediction > 0.5;
 C = confusion(Pd, predicted_Pd);
-<<<<<<< HEAD
-%% P(pd = 1|Xh = M)
-  display('Compute the P(pd = 1|Xh = M) : ');
-  model.predict(1, ['-' 'M' '-'])
-  display('Press Enter to continue ... ');
-  pause;
-%% __________________________ Parameters learned __________________________
-%
-display('--------------------- Parameters learned ---------------------');
-display(P_Xb_given_Pd);
-display(P_Xh_given_Pd);
-display(P_Xt_given_Pd);
-%% Confusion matrix
-evidence = [Xb Xh Xt];
-data_size = size(evidence, 1);
-prediction = zeros(data_size, 1);
+% accuracy rate = sum of diagonal / sum of all
+accuracy = sum(diag(C)) / sum(sum(C));
 
-model = BN1.model(P_Pd, P_Xb_given_Pd, P_Xh_given_Pd, P_Xt_given_Pd);
-for i = 1:data_size
-    prediction(i, 1) = model.predict(1, evidence(i, :));
-end
-% normalize the prediction 
-%   the person drink (Pd = 1) if prediction > 0.5,
-%   otherwise, the person doesn't drink (Pd = 0)
-predicted_Pd = prediction > 0.5;
-C = confusion(Pd, predicted_Pd);
 display(' ');
-%% __________________________ Confusion matrix  ___________________________
-%
 display('--------------------- Confusion matrix ---------------------');
 display(C);
-=======
-
->>>>>>> origin/master
-display(' ');
-display('------------------- Compute the Accuracy -------------------');
-display(' ');
-sum=C(1,1)+C(1,2)+C(2,1)+C(2,2);
-acc=((C(1,1)+C(2,2))/sum)*100
+display(accuracy);
 display('Press Enter to continue ... ');
-<<<<<<< HEAD
 pause;
-=======
-pause;
->>>>>>> origin/master
