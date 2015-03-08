09a5a5509797b9a97fb5c7998ba6590037198036
adilalazzawi <alazzawi1979@gmail.com>
2015-03-08 12:37:30 -0500

    Adil's update

--- BN1.m
+++ BN1.m
@@ -3,28 +3,28 @@
 clc;
 display('_______________________________________________________');
 display('                                                       ');
-display('             Programming Assignment #2                 ');
+display('                Programming Assignment #2              ');
+fprintf('                  Data set No: %d\n', d);
+display('_______________________________________________________');
+display(' ');
+display('_______________________________________________________');
+display('                                                       ');
 display('               Bayesian Networks No.1                  ');
 display('_______________________________________________________');
 display(' ');
-%% Load the data set...
-Pd = dataset1001(:, 5);
-%% Get the discretize
-if (x==1)
-   Xb = discretize(dataset1001(:, 1));
-   Xh = discretize(dataset1001(:, 2));
-   Xt = discretize(dataset1001(:, 3));
-elseif (x==2)   
-   Xb = discretize(dataset1004(:, 1));
-   Xh = discretize(dataset1004(:, 2));
-   Xt = discretize(dataset1004(:, 3));
-end
-%% Generate probabilities
+%% Load the data and discretize
+Pd = dataset(:, 5);
+Xb = discretize(dataset(:, 1));
+Xh = discretize(dataset(:, 2));
+Xt = discretize(dataset(:, 3));
+
+%% Learn parameters of the CPDs
 P_Pd = [sum(Pd == 1)/length(Pd); sum(Pd == 0)/length(Pd)];
 P_Xb_given_Pd = BN1.CPT(Xb, Pd);
 P_Xh_given_Pd = BN1.CPT(Xh, Pd);
 P_Xt_given_Pd = BN1.CPT(Xt, Pd);
 
+%<<<<<<< HEAD
 %% Predict P(Pd|Xb, Xh, Xt)...
    model = BN1.model(P_Pd, P_Xb_given_Pd, P_Xh_given_Pd, P_Xt_given_Pd);
 
@@ -41,3 +41,29 @@ P_Xt_given_Pd = BN1.CPT(Xt, Pd);
   model.predict(1, ['-' 'M' '-'])
   display('Press Enter to continue ... ');
   pause;
+%% __________________________ Parameters learned __________________________
+%
+display('--------------------- Parameters learned ---------------------');
+display(P_Xb_given_Pd);
+display(P_Xh_given_Pd);
+display(P_Xt_given_Pd);
+%% Confusion matrix
+evidence = [Xb Xh Xt];
+data_size = size(evidence, 1);
+prediction = zeros(data_size, 1);
+
+model = BN1.model(P_Pd, P_Xb_given_Pd, P_Xh_given_Pd, P_Xt_given_Pd);
+for i = 1:data_size
+    prediction(i, 1) = model.predict(1, evidence(i, :));
+end
+% normalize the prediction 
+%   the person drink (Pd = 1) if prediction > 0.5,
+%   otherwise, the person doesn't drink (Pd = 0)
+predicted_Pd = prediction > 0.5;
+C = confusion(Pd, predicted_Pd);
+display(' ');
+%% __________________________ Confusion matrix  ___________________________
+%
+display('--------------------- Confusion matrix ---------------------');
+display(C);
+pause;
--- BN3.m
+++ BN3.m
@@ -1,4 +1,4 @@
-% Bayesian Networks No.3...
+% Bayesian Networks No.3
 %%
 display('_______________________________________________________');
 display('                                                       ');
@@ -29,3 +29,41 @@ display(' ');
 % P(pd = 1|Xb = 9.3, Xh = 78.4, Xt = 36.3)
   display('Compute the P(pd = 1|Xb = 9.3, Xh = 78.4, Xt = 36.3) :'); 
   model.predict(1, [9.3 78.4 36.3])
+%% Load the data
+Pd = dataset(:, 5);
+Xb = dataset(:, 1);
+Xh = dataset(:, 2);
+Xt = dataset(:, 3);
+
+%% Learn parameters of the CPDs
+P_Pd = [sum(Pd == 1)/length(Pd); sum(Pd == 0)/length(Pd)];
+P_Xb_given_Pd = mle(Xb, Pd);
+P_Xh_given_Pd = mle(Xh, Pd);
+P_Xt_given_Pd = mle(Xt, Pd);
+
+display('--------------------- Parameters learned ---------------------');
+display(P_Xb_given_Pd);
+display(P_Xh_given_Pd);
+display(P_Xt_given_Pd);
+
+%% Confusion matrix
+evidence = [Xb Xh Xt];
+data_size = size(evidence, 1);
+prediction = zeros(data_size, 1);
+
+model = BN3.model(P_Pd, P_Xb_given_Pd, P_Xh_given_Pd, P_Xt_given_Pd);
+for i = 1:data_size
+    prediction(i, 1) = model.predict(1, evidence(i, :));
+end
+% normalize the prediction 
+%   the person drink (Pd = 1) if prediction > 0.5,
+%   otherwise, the person doesn't drink (Pd = 0)
+predicted_Pd = prediction > 0.5;
+C = confusion(Pd, predicted_Pd);
+
+display(' ');
+display('--------------------- Confusion matrix ---------------------');
+display(C);
+
+%%
+pause;
