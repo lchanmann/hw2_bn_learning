91b8801470320813046a59f7e31db79d47a105d3
Mann <chanmannlim@gmail.com>
2015-03-08 12:08:40 -0500

    fix merge error

--- BN1.m
+++ BN1.m
@@ -7,37 +7,40 @@ display('             Programming Assignment #2                 ');
 display('               Bayesian Networks No.1                  ');
 display('_______________________________________________________');
 display(' ');
-%% Load the data set...
-Pd = dataset1001(:, 5);
-%% Get the discretize
-if (x==1)
-   Xb = discretize(dataset1001(:, 1));
-   Xh = discretize(dataset1001(:, 2));
-   Xt = discretize(dataset1001(:, 3));
-elseif (x==2)   
-   Xb = discretize(dataset1004(:, 1));
-   Xh = discretize(dataset1004(:, 2));
-   Xt = discretize(dataset1004(:, 3));
-end
-%% Generate probabilities
+
+%% Load the data and discretize
+Pd = dataset(:, 5);
+Xb = discretize(dataset(:, 1));
+Xh = discretize(dataset(:, 2));
+Xt = discretize(dataset(:, 3));
+
+%% Learn parameters of the CPDs
 P_Pd = [sum(Pd == 1)/length(Pd); sum(Pd == 0)/length(Pd)];
 P_Xb_given_Pd = BN1.CPT(Xb, Pd);
 P_Xh_given_Pd = BN1.CPT(Xh, Pd);
 P_Xt_given_Pd = BN1.CPT(Xt, Pd);
 
-%% Predict P(Pd|Xb, Xh, Xt)...
-   model = BN1.model(P_Pd, P_Xb_given_Pd, P_Xh_given_Pd, P_Xt_given_Pd);
+display('--------------------- Parameters learned ---------------------');
+display(P_Xb_given_Pd);
+display(P_Xh_given_Pd);
+display(P_Xt_given_Pd);
 
-%% P(pd = 1|Xb = M, Xh = M, Xt = M)...
-  display('Compute the P(pd = 1|Xb = M, Xh = M, Xt = M) :');
-  model.predict(1, ['M' 'M' 'M'])
+%% Confusion matrix
+evidence = [Xb Xh Xt];
+data_size = size(evidence, 1);
+prediction = zeros(data_size, 1);
 
-%% P(pd = 1) without any evidences
-  display('Compute the P(pd = 1) without any evidences : ');
-  model.predict(1, ['-' '-' '-'])
+model = BN1.model(P_Pd, P_Xb_given_Pd, P_Xh_given_Pd, P_Xt_given_Pd);
+for i = 1:data_size
+    prediction(i, 1) = model.predict(1, evidence(i, :));
+end
+% normalize the prediction 
+%   the person drink (Pd = 1) if prediction > 0.5,
+%   otherwise, the person doesn't drink (Pd = 0)
+predicted_Pd = prediction > 0.5;
+C = confusion(Pd, predicted_Pd);
 
-%% P(pd = 1|Xh = M)
-  display('Compute the P(pd = 1|Xh = M) : ');
-  model.predict(1, ['-' 'M' '-'])
-  display('Press Enter to continue ... ');
-  pause;
+display(' ');
+display('--------------------- Confusion matrix ---------------------');
+display(C);
+pause;
